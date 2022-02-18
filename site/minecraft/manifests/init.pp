d589a16583a2b482744ffae93d3ab9f1657842f4
systemville <99295699+systemville@users.noreply.github.com>
2022-02-18 16:12:30 -0500

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -17,5 +17,9 @@ class minecraft {
     ensure => file,
     source => 'puppet:///modules/minecraft/minecraft.service',
   }
+  service {'minecraft':
+    ensure => running,
+    enable => true,
+  }
 }
 
